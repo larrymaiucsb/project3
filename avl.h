b65d57e8e0083a09c4dd910bf76115328d444fbe
larrymaiucsb <larrymai@ucsb.edu>
2020-12-15 23:57:01 +0000

    p

---
+++ avl.h
@@ -0,0 +1,16 @@
+#include <iostream>
+
+class avl{
+  private:
+    struct Node{
+      int num;
+      int decimal;
+
+      Node*  left;
+      Node* right;
+      int height;
+
+      
+    }
+
+}
